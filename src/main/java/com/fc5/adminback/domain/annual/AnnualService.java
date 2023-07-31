419e4feef9f3c0c7388865932820083d324f8a27
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-07-31 20:46:35 +0900

    [#6] feat: 연차 조회 페이지네이션 구현

--- src/main/java/com/fc5/adminback/domain/annual/AnnualService.java
+++ src/main/java/com/fc5/adminback/domain/annual/AnnualService.java
@@ -2,17 +2,20 @@ package com.fc5.adminback.domain.annual;
 
 import com.fc5.adminback.domain.model.Annual;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AnnualService {
 
     private final AnnualRepository annualRepository;
-    public List<Annual> getAll() {
-        return annualRepository.findAll();
+    public Page<Annual> getAll(int page) {
+        return annualRepository.findAll(PageRequest.of(page - 1, 2, Sort.by(
+                Sort.Order.desc("status"),
+                Sort.Order.desc("createdAt")
+        )));
     }
 }
