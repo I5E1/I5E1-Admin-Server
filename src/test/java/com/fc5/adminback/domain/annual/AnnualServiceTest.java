14caa8c73ad198592a42a685f85e49aa3e25b0fc
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-01 15:22:42 +0900

    [#6] test: 연차 조회 서비스 테스트 구현

---
+++ src/test/java/com/fc5/adminback/domain/annual/AnnualServiceTest.java
@@ -0,0 +1,44 @@
+package com.fc5.adminback.domain.annual;
+
+import com.fc5.adminback.domain.model.Annual;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+class AnnualServiceTest {
+
+    @InjectMocks
+    private AnnualService annualService;
+
+    @Mock
+    private AnnualRepository annualRepository;
+
+    @Test
+    @DisplayName("모든 연차조회 및 페이징 테스트")
+    void getAllAnualTest() {
+        // given
+        int pageIndex = 3;
+        Page<Annual> mockPage = new MockPage(pageIndex);
+        given(annualRepository.findAll(any(PageRequest.class))).willReturn(mockPage);
+
+        // when
+        Page<Annual> page = annualService.getAll(3);
+
+        // then
+        assertThat(page.getNumber()).isEqualTo(3);
+        assertThat(page.getNumberOfElements()).isEqualTo(10);
+    }
+}
---
+++ src/test/java/com/fc5/adminback/domain/annual/MockPage.java
@@ -0,0 +1,98 @@
+package com.fc5.adminback.domain.annual;
+
+import com.fc5.adminback.domain.model.Annual;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Function;
+
+public class MockPage implements Page<Annual> {
+
+    private final int pageIndex;
+    public MockPage(int pageIndex) {
+        this.pageIndex = pageIndex;
+    }
+
+    @Override
+    public int getTotalPages() {
+        return 0;
+    }
+
+    @Override
+    public long getTotalElements() {
+        return 0;
+    }
+
+    @Override
+    public Page map(Function converter) {
+        return null;
+    }
+
+    @Override
+    public int getNumber() {
+        return pageIndex;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public int getNumberOfElements() {
+        return 10;
+    }
+
+    @Override
+    public List getContent() {
+        return null;
+    }
+
+    @Override
+    public boolean hasContent() {
+        return false;
+    }
+
+    @Override
+    public Sort getSort() {
+        return null;
+    }
+
+    @Override
+    public boolean isFirst() {
+        return false;
+    }
+
+    @Override
+    public boolean isLast() {
+        return false;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public boolean hasPrevious() {
+        return false;
+    }
+
+    @Override
+    public Pageable nextPageable() {
+        return null;
+    }
+
+    @Override
+    public Pageable previousPageable() {
+        return null;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
