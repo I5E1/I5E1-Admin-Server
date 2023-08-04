701224168821a958da674b4afe5e018902a81b0d
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-04 15:35:25 +0900

    [#27] fix: MockSession을 사용하여 테스트 수정

--- src/test/java/com/fc5/adminback/domain/annual/AnnualControllerTest.java
+++ src/test/java/com/fc5/adminback/domain/annual/AnnualControllerTest.java
@@ -1,14 +1,17 @@
 package com.fc5.adminback.domain.annual;
 
 import com.fc5.adminback.domain.model.Annual;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.validation.BindException;
 
@@ -20,8 +23,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @DisplayName("AnnualController 테스트")
-@SpringBootTest
 @AutoConfigureMockMvc
+@SpringBootTest
 class AnnualControllerTest {
 
     @Autowired
@@ -30,17 +33,28 @@ class AnnualControllerTest {
     @MockBean
     private AnnualService annualService;
 
+//    private final MockHttpSession httpSession = new MockHttpSession();
+//
+//    @BeforeEach
+//    public void clear() {
+//        httpSession.clearAttributes();
+//    }
+
+
     @DisplayName("[/api/annual?page=int.class] 모든 연차 조회 컨트롤러 테스트 - 정상 응답")
     @Test
     public void getAllAnnualTest() throws Exception {
         // given
         Page<Annual> page = new MockPage(1);
         given(annualService.getAll(1)).willReturn(page);
+        MockHttpSession httpSession = new MockHttpSession();
+        httpSession.setAttribute("adminId", 1L);
 
         // when
         mockMvc.perform(
                         get("/api/annual")
                                 .param("page", Integer.toString(1))
+                                .session(httpSession)
                 )
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
@@ -57,11 +71,14 @@ class AnnualControllerTest {
     @Test
     public void getAllAnnualWithInvalidExceptionTest() throws Exception {
         // given
+        MockHttpSession httpSession = new MockHttpSession();
+        httpSession.setAttribute("adminId", 1L);
 
         // when
         mockMvc.perform(
                         get("/api/annual")
                                 .param("page", Integer.toString(0))
+                                .session(httpSession)
                 )
                 .andExpect(status().isBadRequest())
                 .andExpect(result -> assertThat(result.getResolvedException()).isInstanceOf(BindException.class));
