0220e0a4fb1a89a65d16bd285d078dd7671fef75
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-04 19:18:04 +0900

    [#22] refactor: Duty Controller 분리

--- src/main/java/com/fc5/adminback/domain/duty/DutyService.java
+++ src/main/java/com/fc5/adminback/domain/duty/DutyService.java
@@ -1,6 +1,6 @@
 package com.fc5.adminback.domain.duty;
 
-import com.fc5.adminback.domain.model.Annual;
+import com.fc5.adminback.domain.member.MemberRepository;
 import com.fc5.adminback.domain.model.Duty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -9,7 +9,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,7 +19,28 @@ public class DutyService {
 
     private final DutyRepository dutyRepository;
 
-    public Page<Duty> getAll(int page) {
+    public DutyPagingResponseDto getAll(int page) {
+
+        Page<Duty> pages = getPages(page);
+
+        int totalPage = pages.getTotalPages();
+        List<DutyResponseDto> result = getPagingResult(pages);
+
+        if (result.size() == 0) {
+            throw new IllegalArgumentException("더 이상 당직 정보가 존재하지 않습니다.");
+        }
+
+        return DutyPagingResponseDto.of(result, page, totalPage);
+
+    }
+
+    private List<DutyResponseDto> getPagingResult(Page<Duty> pages) {
+        return pages.stream()
+                .map(DutyResponseDto::of)
+                .collect(Collectors.toList());
+    }
+
+    private Page<Duty> getPages(int page) {
         return dutyRepository.findAll(PageRequest.of(page - 1, 2, Sort.by(
                 Sort.Order.desc("status"),
                 Sort.Order.desc("createdAt")
@@ -30,13 +52,17 @@ public class DutyService {
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 당직 정보입니다"));
     }
 
-    public void update(Duty duty, UpdateDutyRequestDto updateDutyRequestDto) {
+    public void update(Long dutyId, UpdateDutyRequestDto updateDutyRequestDto) {
+
+        Duty duty = get(dutyId);
         duty.updateByRequest(updateDutyRequestDto);
 
         dutyRepository.save(duty);
     }
 
-    public void delete(Duty duty) {
+    public void delete(Long dutyId) {
+
+        Duty duty = get(dutyId);
         dutyRepository.delete(duty);
     }
 }
