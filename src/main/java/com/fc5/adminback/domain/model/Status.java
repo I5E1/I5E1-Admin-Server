602f10c4fc2c6c5b3daf265a01498b8480cccc99
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-08 02:23:05 +0900

    [#34] fix: API 수정에 따른 Status.description 변경

--- src/main/java/com/fc5/adminback/domain/model/Status.java
+++ src/main/java/com/fc5/adminback/domain/model/Status.java
@@ -5,10 +5,10 @@ import lombok.Getter;
 @Getter
 public enum Status {
 
-    REQUESTED("신청 중"),
-    APPROVED("승인 완료"),
-    CANCELED("취소 완료"),
-    REJECTED("요청 반려"),
+    REQUESTED("미승인"),
+    APPROVED("승인"),
+    CANCELED("취소"),
+    REJECTED("반려"),
     COMPLETED("완료");
 
     private final String description;
