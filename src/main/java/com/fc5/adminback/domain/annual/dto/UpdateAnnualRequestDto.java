0e57c6d796c6930bb8d447c5575775df6484312c
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-07 16:03:55 +0900

    [#30] feat: dto에 Java Bean Validation 어노테이션 추가

--- src/main/java/com/fc5/adminback/domain/annual/dto/UpdateAnnualRequestDto.java
+++ src/main/java/com/fc5/adminback/domain/annual/dto/UpdateAnnualRequestDto.java
@@ -1,19 +1,35 @@
 package com.fc5.adminback.domain.annual.dto;
 
+import com.fc5.adminback.common.validator.annotation.DateConfirm;
 import com.fc5.adminback.domain.model.Status;
 import lombok.Getter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Getter
 @ToString
+@DateConfirm
 public class UpdateAnnualRequestDto {
 
+    @NotNull
     private LocalDate startDate;
-    private LocalDate endDate;
-    private String summary;
-    private String reason;
-    private Status status;
 
+    @NotNull
+    private LocalDate endDate;
+
+    @NotNull
+    @NotEmpty
+    private String summary;
+
+    @NotEmpty
+    @NotNull
+    @Length(min = 1, max = 20)
+    private String reason;
+
+    @NotNull
+    private Status status;
 }
