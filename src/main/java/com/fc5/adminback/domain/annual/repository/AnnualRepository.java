732330a9780c00cb87741a88e97a839854f6583f
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-07 17:03:20 +0900

    [#30] feat: 겹치는 연차 기간에 대한 예외 처리

--- src/main/java/com/fc5/adminback/domain/annual/repository/AnnualRepository.java
+++ src/main/java/com/fc5/adminback/domain/annual/repository/AnnualRepository.java
@@ -2,6 +2,23 @@ package com.fc5.adminback.domain.annual.repository;
 
 import com.fc5.adminback.domain.model.Annual;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface AnnualRepository extends JpaRepository<Annual, Long> {
+
+    @Query("SELECT a FROM Annual a " +
+            "WHERE " +
+            "a.member.id = :memberId and " +
+            "(a.startDate <= :startDate and :startDate <= a.endDate) " +
+            "or " +
+            "(a.startDate <= :endDate and :endDate <= a.endDate)")
+    List<Annual> findAnnualByInvalidDate(
+            @Param("memberId") Long memberId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate
+    );
 }
