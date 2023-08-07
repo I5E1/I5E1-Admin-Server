adcb41b1a1163d9cb431542e8a8595ab21dff9c6
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-07 16:03:55 +0900

    [#30] feat: dto에 Java Bean Validation 어노테이션 추가

--- src/main/java/com/fc5/adminback/domain/admin/dto/UpdateUserPositionDto.java
+++ src/main/java/com/fc5/adminback/domain/admin/dto/UpdateUserPositionDto.java
@@ -3,8 +3,11 @@ package com.fc5.adminback.domain.admin.dto;
 import com.fc5.adminback.domain.model.Position;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 public class UpdateUserPositionDto {
 
+    @NotNull
     private Position position;
 }
