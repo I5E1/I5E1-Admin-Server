8439f7e50c0bf3403e0865012cfbaa2e82913cfa
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-07 13:48:30 +0900

    [#29] refactor: AdminController 분리

--- src/main/java/com/fc5/adminback/domain/admin/controller/AdminController.java
+++ src/main/java/com/fc5/adminback/domain/admin/controller/AdminController.java
@@ -31,60 +31,17 @@ public class AdminController {
     @PostMapping("api/login")
     public ResponseEntity<?> login(@RequestBody AdminLoginRequestDto adminLoginRequestDto, HttpServletRequest request) {
         Admin admin = adminService.login(adminLoginRequestDto);
-        request.getSession().invalidate();
-        HttpSession session = request.getSession(true);
 
-        session.setAttribute("adminId", admin.getId());
-        session.setMaxInactiveInterval(3600);
+        adminService.setSession(request, admin);
+
 
         return APIDataResponse.empty(HttpStatus.OK, "로그인에 성공하였습니다");
     }
 
     @GetMapping("api/logout")
     public ResponseEntity<?> logout(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate();
-        }
+        adminService.logout(request);
 
         return APIDataResponse.empty(HttpStatus.OK, "로그아웃에 성공하였습니다");
-    }
-
-    @GetMapping("api/user")
-    public ResponseEntity<?> getPagingMemberList(
-            @ModelAttribute @Valid PageIndex pageIndex
-    ) {
-        int pageSize = 3;
-        int totalPages = memberRepository.findAll().size() / pageSize + 1;
-        if (pageIndex.getPage() > totalPages) {
-            throw new IllegalArgumentException("더 이상 당직 정보가 존재하지 않습니다.");
-        }
-
-        PageRequest pageRequest = PageRequest.of(pageIndex.getPage() - 1, pageSize);
-        List<MemberWithCompletedDutyCount> members = memberRepository.getAllMemberWithExecutedDutyCount(pageRequest);
-        MemberWithCompletedDutyCountPagingResponseDto result =
-                MemberWithCompletedDutyCountPagingResponseDto.of(members, totalPages, pageIndex.getPage());
-        return APIDataResponse.of(HttpStatus.OK, "모든 유저 조회에 성공하였습니다.", result);
-    }
-
-    @GetMapping("/api/annual/{userId}")
-    public ResponseEntity<?> updateAnnual(@PathVariable("userId") Long userId, @RequestParam int size) {
-        if (size < 0) {
-            throw new IllegalArgumentException("올바르지 않은 쿼리 입력입니다");
-        }
-        Member member = memberRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다"));
-        member.modifiyAnnualCount(size);
-        memberRepository.save(member);
-        return APIDataResponse.empty(HttpStatus.OK, "회원의 남은 연차 일 수를 수정했습니다.");
-    }
-
-    @PatchMapping("/api/position/{userId}")
-    public ResponseEntity<?> updatePosition(@PathVariable Long userId, UpdateUserPositionDto updateUserPositionDto) {
-        Member member = memberRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다"));
-        member.modifyPosition(updateUserPositionDto.getPosition());
-        memberRepository.save(member);
-        return APIDataResponse.empty(HttpStatus.OK, "회원의 직급을 수정하였습니다.");
     }
 }
--- src/main/java/com/fc5/adminback/domain/admin/service/AdminService.java
+++ src/main/java/com/fc5/adminback/domain/admin/service/AdminService.java
@@ -8,6 +8,9 @@ import com.fc5.adminback.domain.model.Admin;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Service
 @RequiredArgsConstructor
 public class AdminService {
@@ -18,10 +21,29 @@ public class AdminService {
         Admin admin = adminRepository.findByEmail(loginRequestDto.getEmail())
                 .orElseThrow(() -> new UnauthorizedAdminException(AnnualErrorCode.UNAUTHORIZED.getMessage(), AnnualErrorCode.UNAUTHORIZED));
 
+        validatePassword(loginRequestDto, admin);
+
+        return admin;
+    }
+
+    public void logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+    }
+
+    public void setSession(HttpServletRequest request, Admin admin) {
+        request.getSession().invalidate();
+        HttpSession session = request.getSession(true);
+
+        session.setAttribute("adminId", admin.getId());
+        session.setMaxInactiveInterval(3600);
+    }
+
+    private void validatePassword(AdminLoginRequestDto loginRequestDto, Admin admin) {
         if (!admin.getPassword().equals(loginRequestDto.getPassword())) {
             throw new UnauthorizedAdminException(AnnualErrorCode.UNAUTHORIZED.getMessage(), AnnualErrorCode.UNAUTHORIZED);
         }
-
-        return admin;
     }
 }
