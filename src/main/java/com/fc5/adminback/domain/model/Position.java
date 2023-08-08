5909b67d0b4ca4be7df47914d0ef0805047090cd
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-08 18:37:25 +0900

    [#47] fix: 직급 수정 API 에러 해결
    @RequestBody 누락

--- src/main/java/com/fc5/adminback/domain/model/Position.java
+++ src/main/java/com/fc5/adminback/domain/model/Position.java
@@ -3,7 +3,17 @@ package com.fc5.adminback.domain.model;
 public enum Position {
 
     STAFF("사원"),
-    BOSS("사장");
+    BOSS("사장"),
+    INTERN("인턴"),
+    ASSISTANT_MANAGER("대리"),
+    MANAGER("과장"),
+    GENERAL_MANAGER("실장"),
+    DEPUTY_GENERAL_MANAGER("차장"),
+    DEPARTMENT_MANAGER("부장"),
+    MANAGING_DIRECTOR("상무"),
+    SENIOR_MANAGING_DIRECTOR("전무"),
+    CHAIRMAN("회장"),
+    ;
 
     private final String type;
 
