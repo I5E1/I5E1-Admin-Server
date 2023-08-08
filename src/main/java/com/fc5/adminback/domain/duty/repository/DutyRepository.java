702e1d60ce27b47f91010f19d476394fae47b52f
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-08 20:34:09 +0900

    [#46] fix: 당직 정렬 쿼리 수정

--- src/main/java/com/fc5/adminback/domain/duty/repository/DutyRepository.java
+++ src/main/java/com/fc5/adminback/domain/duty/repository/DutyRepository.java
@@ -1,7 +1,20 @@
 package com.fc5.adminback.domain.duty.repository;
 
+import com.fc5.adminback.domain.model.Annual;
 import com.fc5.adminback.domain.model.Duty;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DutyRepository extends JpaRepository<Duty, Long> {
+    @Query("SELECT d FROM Duty d " +
+            "ORDER BY " +
+            "CASE " +
+            "WHEN d.status = 'REQUESTED' THEN 0 " +
+            "WHEN d.status = 'APPROVED' THEN 1 " +
+            "WHEN d.status = 'CANCELED' THEN 2 " +
+            "WHEN d.status = 'REJECTED' THEN 3 " +
+            "ELSE 4 END")
+    Page<Duty> findAllWithOrder(PageRequest pageRequest);
 }
